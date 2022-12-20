47a82f33c25361deea13a42370865574c96947ae
PY Verret <pverret@gmail.com>
2022-12-20 11:28:32 -0500

    add query

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-export default function isTrue (bool) {
+export function isTrue (bool) {
     return bool ? 'This is True' : 'This is False';
 }
+
+export function getSomething (str = '1') {
+    fetch('https://pokeapi.co/api/v2/berry/' + str + '/').then(function (response) {
+        console.log(response);
+        return response;
+    });
+} 
